978dbbb2a0c7f922c438627e33e1d51643e66598
cerdelen <95369756+cerdelen@users.noreply.github.com>
2022-05-17 23:07:03 +0200

    Update case_iord.c

--- case_iord.c
+++ case_iord.c
@@ -12,15 +12,19 @@
 
 #include "ft_printf.h"
 
-int	print_int(int i)
+int	print_int(long int i)
 {
 	int		counter;
 
+
+	if (i < 0)
+		i = -i;
 	counter = 0;	
 	if (i >= 10)
-		counter = print_hex(i / 10);
+		counter = print_int(i / 10);
 	return (counter + write(1, &"0123456789"[i % 10], 1));
 }
+
 
 int	case_iord(va_list *args)
 {
